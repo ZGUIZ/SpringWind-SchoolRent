624f0e225731051ebbf980c40eef29d0bc4b10dd
zguiz <RFToona@outlook.com>
2019-04-12 20:08:43 +0800

    完成消息功能

--- src/main/java/com/baomidou/springwind/quartz/DailyCal.java
+++ src/main/java/com/baomidou/springwind/quartz/DailyCal.java
@@ -5,6 +5,7 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import java.text.SimpleDateFormat;
 
@@ -13,8 +14,9 @@ public class DailyCal implements Job {
     @Autowired
     private IRentService service;
 
-    @Override
+     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
         System.out.println("统计任务-->"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
         service.calRentalDaily();
         System.out.println("统计任务结束-->"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
