f24660f56609b5a6e30361027cd043ddee6e3aae
zguiz <RFToona@outlook.com>
2019-04-24 18:01:44 +0800

    完善消息推送

--- src/main/java/com/baomidou/springwind/controller/SecondResponseInfoController.java
+++ src/main/java/com/baomidou/springwind/controller/SecondResponseInfoController.java
@@ -4,6 +4,8 @@ import com.baomidou.springwind.entity.ResponseInfo;
 import com.baomidou.springwind.entity.Result;
 import com.baomidou.springwind.entity.SecondResponseInfo;
 import com.baomidou.springwind.entity.Student;
+import com.baomidou.springwind.service.IMessageService;
+import com.baomidou.springwind.service.IResponseInfoService;
 import com.baomidou.springwind.service.ISecondResponseInfoService;
 import com.baomidou.springwind.utils.UUIDUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +34,13 @@ public class SecondResponseInfoController {
 
     @Autowired
     private ISecondResponseInfoService secondResponseInfoService;
+
+    @Autowired
+    private IMessageService messageService;
+
+    @Autowired
+    private IResponseInfoService responseInfoService;
+
     /**
      * 添加
      * @param request
@@ -42,18 +51,8 @@ public class SecondResponseInfoController {
     public Result addResponseInfo(HttpServletRequest request, @RequestBody SecondResponseInfo secondResponseInfo){
         Result result = new Result();
         HttpSession httpSession = request.getSession();
-        if(httpSession == null){
-            result.setResult(false);
-            result.setMsg("用户未登录!");
-            return result;
-        }
         //从Session中获取用户信息
         Student student = (Student) httpSession.getAttribute("student");
-        if(student == null){
-            result.setResult(false);
-            result.setMsg("用户未登录!");
-            return result;
-        }
         try{
             secondResponseInfo.setResponseInfo(URLDecoder.decode(secondResponseInfo.getResponseInfo(),"utf-8"));
             secondResponseInfo.setResponseId(UUIDUtil.getUUID());
@@ -68,6 +67,15 @@ public class SecondResponseInfoController {
             } else {
                 result.setResult(false);
                 result.setMsg("回复失败");
+            }
+
+            //推送信息
+            ResponseInfo responseInfo = responseInfoService.selectById(secondResponseInfo.getParentId());
+            if(responseInfo!=null){
+                StringBuffer sb = new StringBuffer("@");
+                sb.append(student.getUserId()).append("回复了您的消息\"").append(responseInfo.getResponseInfo()).append("\"");
+                sb.append("内容为：").append(secondResponseInfo.getResponseInfo());
+                messageService.pushMessage("有人回复了您",sb.toString(),responseInfo.getUserId());
             }
         }catch (Exception e){
             e.printStackTrace();
